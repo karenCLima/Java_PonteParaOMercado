9362a937478ab07f95d4b1c92e25628ee13b03b5
karenCLima <lima.kcls@gmail.com>
2024-06-21 15:47:28 -0300

    feat: dto-parte2

--- src/main/java/Java_PonteParaOMercado/dto/ProdutoRequest.java
+++ src/main/java/Java_PonteParaOMercado/dto/ProdutoRequest.java
@@ -1,5 +1,25 @@
 package Java_PonteParaOMercado.dto;
 
+import java.math.BigDecimal;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor @NoArgsConstructor
+@Getter @Setter
 public class ProdutoRequest {
+	
+	@NotBlank
+	private String nome;
+	@NotBlank
+	private String marca;
+	@DecimalMin(value = "0.0", inclusive = false)
+	private BigDecimal preco;
+	
+	private String descricao;
 
 }
---
+++ src/main/java/Java_PonteParaOMercado/utils/ProdutoConvert.java
@@ -0,0 +1,41 @@
+package Java_PonteParaOMercado.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Java_PonteParaOMercado.dto.ProdutoRequest;
+import Java_PonteParaOMercado.dto.ProdutoResponse;
+import Java_PonteParaOMercado.model.Produto;
+
+public class ProdutoConvert {
+	
+	public static Produto toEntity(ProdutoRequest produtoRequest) {
+		Produto produto = new Produto();
+		produto.setNome(produtoRequest.getNome());
+		produto.setMarca(produtoRequest.getMarca());
+		produto.setPreco(produtoRequest.getPreco());
+		produto.setDescricao(produtoRequest.getDescricao());
+		return produto;
+	}
+	
+	public static ProdutoResponse toResponse(Produto produto) {
+		ProdutoResponse produtoResponse = new ProdutoResponse();
+		produtoResponse.setId(produto.getId());
+		produtoResponse.setNome(produto.getNome());
+		produtoResponse.setMarca(produto.getMarca());
+		produtoResponse.setPreco(produto.getPreco());
+		produtoResponse.setDescricao(produto.getDescricao());
+		return produtoResponse;
+	}
+	
+	public static List<ProdutoResponse> toResponseList(List<Produto> produtos){
+		List<ProdutoResponse> produtoResponses = new ArrayList<>();
+		for(Produto produto: produtos) {
+			ProdutoResponse produtoResponse = ProdutoConvert.toResponse(produto);
+			produtoResponses.add(produtoResponse);
+		}
+		
+		return produtoResponses;
+	}
+
+}
