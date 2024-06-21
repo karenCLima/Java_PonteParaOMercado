51141c35371ea07d4d77bb1e052e6c82d0dd0f01
karenCLima <lima.kcls@gmail.com>
2024-06-21 15:17:43 -0300

    iniciando o repositorio

---
+++ src/main/java/Java_PonteParaOMercado/JavaPonteParaOMercadoApplication.java
@@ -0,0 +1,13 @@
+package Java_PonteParaOMercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPonteParaOMercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPonteParaOMercadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Java_PonteParaOMercado/JavaPonteParaOMercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package Java_PonteParaOMercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPonteParaOMercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
